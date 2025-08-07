50fbc729a2a7cc3d67613e0943a5e9115301acf5
AliReza <alirezatnalireza1@gmail.com>
2025-08-07 18:43:01 +0330

    header

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+       fontFamily: {
+          vazir: ['Vazir', 'sans-serif'],
+       },
+        colors: {
+            bklogo : "#fbfaf8" ,
+          }
+    },
+  },
+  plugins: [],
+}
+
